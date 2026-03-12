1a0874452bf72d7108fbee8c7aa6ecb80f112730
Henry-Tercero-MH <hterceroh@miumg.edu.gt>
2026-03-12 17:12:06 -0600

    merge

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -9,14 +9,18 @@ export default function NotFound() {
         <div className="w-64 h-64 rounded-full bg-white flex flex-col items-center justify-center border-4 border-primary shadow-lg">
           <img
             src={logo}
-            alt="Logo AUTO+"
+            alt="Logo AUTO+ Mecánica General"
             className="w-56 h-44 object-contain mb-0"
           />
-          <p className="text-sm font-bold uppercase tracking-wide text-slate-700 mt-1"></p>
+          {/* Nombre del usuario */}
         </div>
       </div>
       <h1 className="text-6xl font-black text-accent mb-6">404</h1>
-      <p className="text-slate-500 mb-6 text-center">Ruta no encontrada.<br />Comuniquese con el desarrollador.</p>
+      <p className="text-slate-500 mb-6 text-center">
+        Ruta no encontrada.<br />
+        Comuniquese con el desarrollador.<br />
+        <a href="https://wa.me/50240705002" className="text-blue-600 underline">WhatsApp</a>
+      </p>
       <Link to="/" className="bg-primary text-white px-6 py-2 rounded-xl font-bold shadow hover:bg-blue-700 transition">Ir al inicio</Link>
     </div>
   );
